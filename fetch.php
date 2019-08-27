622be400531452f5097c5aaaa15a6a8da2c22711
root <root@lamp>
2019-08-27 11:03:50 -0400

    version final

---
+++ fetch.php
@@ -0,0 +1,130 @@
+<?php
+
+date_default_timezone_set('America/Santiago'); 
+//$date=date("Y-m-d");
+
+if(isset($_POST['query']) && $_POST['query']>0){
+    $date = date('Y-m-d', strtotime($date. ' + '.$_POST['query'].' days'));
+    echo $date;
+}else if(isset($_POST['query']) && $_POST['query']<0){
+    $date = date('Y-m-d', strtotime($date. ' - '.abs($_POST['query']).' days'));
+    echo $date;
+}else 
+{
+    $date=date("Y-m-d");
+    echo $date;
+}
+
+$diadelasemana = date("w", strtotime($date)); //obtiene el dia de la semana en valor numerico de domingo a sabado - 0 a 6
+$fechainicial = date_create('2019-07-29');
+$fechaactual = date_create($date);
+$interval = date_diff($fechainicial, $fechaactual);
+$week = $interval->format('%a') / 7;
+$week = (round($week,1)); 
+
+while($week > 8)
+{
+    $week = $week - 8;
+}
+$especialista = array("A","AA","B","BB","C","CC","D","DD");
+
+$url = 'turnositc.json'; // ruta del archivo JSON con los turnos de 8 semanas
+$data = file_get_contents($url); // se abre el archivo JSON para lectura
+$turnositc = json_decode($data); // interpretacion de la informacion del archivo JSON
+$turnos = explode(",",$turnositc[$week]->$diadelasemana); //separacion de los turnos del dia, se usa la coma como caracter de separacion
+
+$turnoM = array();
+$turnoT = array();
+$turnoN = array();
+$turnoMN = array();
+$turnoD = array();
+$libre = array();
+
+for($i=0;$i<8;$i++){
+
+    if($turnos[$i]=="M"){
+        array_push($turnoM, $especialista[$i]);
+    }elseif ($turnos[$i]=="T"){
+        array_push($turnoT, $especialista[$i]);
+    }elseif($turnos[$i]=="N"){
+        array_push($turnoN, $especialista[$i]);
+    }elseif($turnos[$i]=="MN"){
+        array_push($turnoMN, $especialista[$i]);
+    }elseif($turnos[$i]=="D"){
+        array_push($turnoD, $especialista[$i]);
+    }elseif($turnos[$i]=="L"){
+        array_push($libre, $especialista[$i]);    
+    }
+}
+
+    if($_SERVER["REQUEST_METHOD"] == "POST") {    
+
+
+       echo '<table class="table table-striped table-bordered table-sm">';
+       echo '<thead class="thead-dark">';
+       echo '<tr><th>Horario</th><th colspan="3">Nombres </th></tr></thead><tr>';
+       echo '<td>07:00 a 14:00</td>';
+       
+        foreach ($turnoM as &$valor) {
+            echo '<td>';
+            echo $valor;
+            echo '</td>';
+        }
+        echo '</tr>';
+        echo '<tr>';
+        echo '<td>';
+        echo '13:00 a 22:00';
+        echo '</td>';
+        foreach ($turnoT as &$valor) {
+            echo '<td>';
+            echo $valor;
+            echo '</td>';
+        }
+        echo '</tr>';
+        echo '<tr>';
+        echo '<td>';
+        echo '22:00 a 08:00';
+        echo '</td>';
+        foreach ($turnoN as &$valor) {
+            echo '<td>';
+            echo $valor;
+            echo '</td>';
+        }
+        echo '</tr>';
+        echo '<tr>';
+        echo '<td>';
+        echo 'MN -';
+        echo '</td>';
+        foreach ($turnoMN as &$valor) {
+            echo '<td>';
+            echo $valor;
+            echo '</td>';
+        }
+        echo '</tr>';
+        echo '<tr>';
+        echo '<td>';
+        echo 'D';
+        echo '</td>';
+        foreach ($turnoD as &$valor) {
+            echo '<td>';
+            echo $valor;
+            echo '</td>';
+        }
+        echo '</tr>';
+        echo '<tr>';
+        echo '<td>';
+        echo 'Dia Libre';
+        echo '</td>';
+        foreach ($libre as &$valor) {
+            echo '<td>';
+            echo $valor;
+            echo '</td>';
+        }
+        echo '</tr>';
+        echo '</table>';
+       
+    
+
+
+    }
+?>
---
+++ turnos.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<?php
+date_default_timezone_set('America/Santiago'); 
+$date=date("Y/m/d");  // guarda la fecha del dia actual
+$diadelasemana = date("w"); //obtiene el dia de la semana en valor numerico de domingo a sabado - 0 a 6
+$especialista = array("Ivan Rosales","Mauricio Aspee","Vacante","Vacante","Gianinna Salazar","Jose Barrios","Derikson Urbano","Christian Peters","Vacante");
+
+//matriz semana es de domingo a sabado - 0 a 6
+$semana = array 
+(
+    array('LI','LI','T1','T2','LI','LI','T3','T1','T31'),
+    array('T3','LI','T2','T1','T2','T31','T1','LI','T1'),
+    array('T3','T2','T21','LI','T2','T3','T1','LI','T1'),
+    array('T3','T2','LI','T1','T2','T3','LI','T2','T1'),
+    array('T3','T2','LI','T1','T2','T3','T31','T2','T1'),
+    array('LI','T2','T1','T1','T21','T3','T3','T21','T1'),
+    array('LI','T21','T1','LI','LI','LI','T3','LI','LI')
+    
+);
+
+$datetime1 = date_create('2018-12-24');
+$datetime2 = date_create($date);
+$interval = date_diff($datetime1, $datetime2);
+$week = $interval->format('%a') / 7;
+$week = round($week / 9 , 1);  
+$week = (($week - floor($week))*10)-1;
+
+$turno1 = array();
+$turno2 = array();
+$turno21 = array();
+$turno3= array();
+$turno31= array();
+$libre  = array();
+
+
+for ($i=0; $i<= 8; $i++){
+
+    if($week == 9){
+    $week=0;
+    }
+  
+    if($semana[$diadelasemana][$week]=="T1"){
+        array_push($turno1, $especialista[$i]);
+    }elseif ($semana[$diadelasemana][$week]=="T2"){
+        array_push($turno2, $especialista[$i]);
+    }elseif($semana[$diadelasemana][$week]=="T21"){
+        array_push($turno21, $especialista[$i]);
+    }elseif($semana[$diadelasemana][$week]=="T3"){
+        array_push($turno3, $especialista[$i]);
+    }elseif($semana[$diadelasemana][$week]=="T31"){
+        array_push($turno31, $especialista[$i]);
+    }elseif($semana[$diadelasemana][$week]=="LI"){
+        array_push($libre, $especialista[$i]);    
+    }
+
+    $week++;
+}
+
+?>
+
+<table class="table table-striped table-bordered table-sm">
+<thead class="thead-dark">
+<tr>
+<th>Horario</th>
+<th colspan="3">Especialistas</th>
+</tr>
+</thead>
+<tr>
+<td>07:00 a 16:00</td>
+
+
+<?php 
+foreach ($turno1 as &$valor) {
+    echo '<td>';
+    echo $valor;
+    echo '</td>';
+}
+echo '</tr>';
+echo '<tr>';
+echo '<td>';
+echo '14:55 a 23:55';
+echo '</td>';
+foreach ($turno21 as &$valor) {
+    echo '<td>';
+    echo $valor;
+    echo '</td>';
+}
+echo '</tr>';
+echo '<tr>';
+echo '<td>';
+echo '15:00 a 24:00';
+echo '</td>';
+foreach ($turno2 as &$valor) {
+    echo '<td>';
+    echo $valor;
+    echo '</td>';
+}
+echo '</tr>';
+echo '<tr>';
+echo '<td>';
+echo '23:00 a 08:00';
+echo '</td>';
+foreach ($turno3 as &$valor) {
+    echo '<td>';
+    echo $valor;
+    echo '</td>';
+}
+echo '</tr>';
+echo '<tr>';
+echo '<td>';
+echo '00:00 a 09:00';
+echo '</td>';
+foreach ($turno31 as &$valor) {
+    echo '<td>';
+    echo $valor;
+    echo '</td>';
+}
+echo '</tr>';
+echo '<tr>';
+echo '<td>';
+echo 'Dia Libre';
+echo '</td>';
+foreach ($libre as &$valor) {
+    echo '<td>';
+    echo $valor;
+    echo '</td>';
+}
+echo '</tr>';
+echo '</table>';
+
+?>
+
+</body>
+</html>
